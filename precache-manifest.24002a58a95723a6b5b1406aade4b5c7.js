9256bc1ee8020598fd6ba85f17230f64619b06ab
nabaanahith <42347859+nabaanahith@users.noreply.github.com>
2021-12-14 17:26:23 +0300

    Add files via upload

---
+++ precache-manifest.24002a58a95723a6b5b1406aade4b5c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc5e9bc99d47dae8c3a8b643d32b3cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb14b590612e4af137dd",
+    "url": "/static/css/2.60674714.chunk.css"
+  },
+  {
+    "revision": "29d311a7fb0b512471a8",
+    "url": "/static/css/main.5a9f3432.chunk.css"
+  },
+  {
+    "revision": "bb14b590612e4af137dd",
+    "url": "/static/js/2.b2b72f9f.chunk.js"
+  },
+  {
+    "revision": "29d311a7fb0b512471a8",
+    "url": "/static/js/main.c2d5b55c.chunk.js"
+  },
+  {
+    "revision": "7ea7979cb78e3fc6e8c8",
+    "url": "/static/js/runtime-main.60e38090.js"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "47ee73e5cb6a0703201aa8a6a145c97e",
+    "url": "/static/media/view.47ee73e5.svg"
+  }
+]);
